628e74a81cd722815709ae3726c7d4deea5e4c63
KGMalith <it18258622@my.sliit.lk>
2021-11-04 09:14:07 +0530

    email sending function created

---
+++ Backend/app/functions/email/sendEmail.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+const config = require('../../../config/config');
+
+module.exports.sendEmail = async (bodyValues) => {
+
+	// eslint-disable-next-line no-useless-catch
+	try{
+		let transporter = nodemailer.createTransport({
+			service: config.Emails.EMAIL_SERVICE,
+			auth: {
+				user: config.Emails.EMAIL_USER,
+				pass: config.Emails.EMAIL_PASSWORD
+			},
+		});
+
+		let mailOptions = {
+			from: config.Emails.EMAIL_USER_NAME,
+			to: bodyValues.receiver_email,
+			subject: bodyValues.email_subject,
+			html: bodyValues.email_body,
+			attachments: bodyValues.attachments
+		};
+		let promiseRespond = await transporter.sendMail(mailOptions);
+
+		return promiseRespond;
+	}catch(err){
+		throw err;
+	}
+	
+};
